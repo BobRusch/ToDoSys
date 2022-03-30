f3108bbfffe2de13fa52111616084ed6725b1d57
Bob Rusch <rvrusch@gmail.com>
2022-03-30 14:10:19 -0400

    CQRS base completed

--- API/Controllers/ToDosController.cs
+++ API/Controllers/ToDosController.cs
@@ -1,27 +1,44 @@
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Persistence;
+using MediatR;
+using Application.Todos;
 
 namespace API.Controllers;
 
 public class ToDosController : BaseApiController
 {
-    private readonly DataContext _context;
-    public ToDosController(DataContext context)
-    {
-        _context = context;
-    }
 
     [HttpGet]
-    public async Task<ActionResult<List<Todo>>> GeToDos()
+    public async Task<ActionResult<List<Todo>>> GetToDos()
     {
-        return await _context.Todos.ToListAsync();
+        return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Todo>> GeToDo(Guid id)
+    public async Task<ActionResult<Todo>> GetToDo(Guid id)
     {
-        return await _context.Todos.FindAsync(id);
+        return await Mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTodo(Todo todo)
+    {
+        return Ok(await Mediator.Send(new Create.Command { Todo = todo }));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditTodo(Guid id, Todo todo)
+    {
+        todo.Id = id;
+        todo.LastUpdated = DateTime.Now;
+
+        return Ok(await Mediator.Send(new Edit.Command { Todo = todo }));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodo(Guid id)
+    {
+        return Ok(await Mediator.Send(new Delete.Command { Id = id }));
     }
 }
